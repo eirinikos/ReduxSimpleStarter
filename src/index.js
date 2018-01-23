4b317180b100fbfb266968c3b3cc88068f094437
Andrea Kao <eirinikos@gmail.com>
2018-01-22 17:18:05 -0800

    [1.8 to 1.11] render React component to DOM
    We now have an index.js file that actually renders
    HTML to the page (i.e., <div>Bonjour!</div>).
    
    In this commit, I've added React and ReactDOM
    import statements to index.js.
    
    NB: we are now also using ES6 syntax in using:
    const App = () => {...}
    
    (instead of: const App = function() {...}

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // create a new component, which will produce HTML
 const App = function() {
 	return <div>Bonjour!</div>;
 }
+
 // show the component's HTML in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
