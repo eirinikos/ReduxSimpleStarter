fbd0d8f78784e9f8d127ea75597e986196096480
Andrea Kao <eirinikos@gmail.com>
2018-01-23 11:12:19 -0800

    [1.14] create and export basic SearchBar component
    We now have a simple tree of components, with
    SearchBar nested inside of App.
    
    The SearchBar component now returns an empty input
    field, which has been exported with `export default
    SearchBar`.
    
    index.js now includes a SearchBar import statement,
    and App's return statement now incorporates ES6
    syntax, which makes it easier to read nested,
    multi-line JSX.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyD6YH1hRhbipszVCI43vfz5kCx8Vr3ugO4';
 
 // create a new component, which will produce HTML
 const App = () => {
-	return <div>Bonjour!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 // show the component's HTML in the DOM
